c310dbe8b20015e244c18a3acf71e0de16f599a3
Grimhiker <66091618+Grimhiker@users.noreply.github.com>
2020-06-10 10:22:21 -0500

    Add files via upload

---
+++ main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+    // test comment
+
+    /*
+    test 
+    multi
+    line
+    comment
+    */
+    println!("{}", s3);
+}
